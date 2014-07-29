c8c0a2cd8189adf3b85d93e635903ad1b4631c16
eloy <eloy.colell@gmail.com>
2014-07-29 13:42:07 -0300

    It removed innecesary code from setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from distutils.core import setup
-from distutils.command.build import build
-from setuptools.command import easy_install
 import os
 import subprocess
 from pip.req import parse_requirements
-from urllib import urlretrieve
-from datetime import datetime
-import sys
 
 
 def calculate_version():
@@ -30,54 +25,6 @@ def calculate_version():
 
 requirements = [str(ir.req) for ir in parse_requirements('requirements.txt')]
 version_git = calculate_version()
-import platform as p
-tmp_path = '/tmp/'
-os_name = p.system()
-binaries = {}
-"""    'hdf5': {
-        'version': '1.8.12',
-        'name': 'hdf5-%s',
-        'url': 'http://www.hdfgroup.org/ftp/HDF5/releases/%s/src',
-        'compile': {
-            'depends': ['numpy==1.8.0'],
-            'config': {
-                'pre': '',
-                'post': '--prefix=/usr/local --enable-shared --enable-hl',
-            },
-        },
-    },
-    'netcdf': {
-        'version': '4.3.1-rc4',
-        'name': 'netcdf-%s',
-        'url': 'ftp://ftp.unidata.ucar.edu/pub/netcdf',
-        'compile': {
-            'depends': [],
-            'config': {
-                'pre': ('LDFLAGS=-L/usr/local/lib '
-                        'CPPFLAGS=-I/usr/local/include '
-                        'LD_LIBRARY_PATH=/usr/local'),
-                'post': ('--enable-netcdf-4 --enable-dap --enable-shared'
-                         ' --prefix=/usr/local'),
-            },
-        },
-    },
-}"""
-systems = {
-    'Linux': {
-        'update_shared_libs': 'sudo ldconfig',
-        'libs': {
-            'hdf5': '/usr/local/lib/libhdf5.so.8.0.1',
-            'netcdf': '/usr/local/lib/libnetcdf.so.7.2.0',
-        },
-    },
-    'Darwin': {
-        'update_shared_libs': '',
-        'libs': {
-            'hdf5': '/usr/local/lib/libhdf5_hl.8.dylib',
-            'netcdf': '/usr/local/lib/libnetcdf.7.dylib',
-        },
-    },
-}
 
 
 def get_long_description():
@@ -94,82 +41,6 @@ def get_long_description():
     except Exception:
         description = open(readme_file).read()
     return description
-
-
-class Builder:
-
-    def __init__(self, lib):
-        self.lib_key = lib
-        self.lib = binaries[lib]
-        self.name = self.lib['name'] % self.lib['version']
-
-    def call(self, cmd):
-        return subprocess.call(cmd, shell=True)
-
-    def download(self):
-        url = self.lib['url']
-        if url.find('%s') > 0:
-            url = url % self.name
-        filename = '%s.tar.gz' % self.name
-        self.local_filename = '%s%s' % (tmp_path, filename)
-        if not os.path.isfile(self.local_filename):
-            begin = datetime.now()
-
-            def dlProgress(count, blockSize, totalSize):
-                transfered = (count * blockSize
-                              if totalSize >= count * blockSize else totalSize)
-                progress = transfered * 100. / totalSize
-                speed = (transfered /
-                         ((datetime.now() - begin).total_seconds())) / 1024
-                print '\r%s' % (' ' * 78),
-                print (u'\rDownloaded %s '
-                       '(\033[33m%03.2f %%\033[0m at \033[35m%i KB/s\033[0m)'
-                       % (filename, progress, speed)),
-                sys.stdout.flush()
-            source = '%s/%s' % (url, filename)
-            destiny = '%s%s' % (tmp_path, filename)
-            self.local_filename, _ = urlretrieve(source, destiny,
-                                                 reporthook=dlProgress)
-
-    def uncompress(self):
-        self.local_unpacked = '%s%s' % (tmp_path, self.name)
-        if not os.path.isdir(self.local_unpacked):
-            self.download()
-            # self.call('rm -rf %s*' % self.local_unpacked)
-            import tarfile as tar
-            tfile = tar.open(self.local_filename, mode='r:gz')
-            tfile.extractall(tmp_path)
-            tfile.close()
-            self.call('chmod -R ugo+rwx %s*' % self.local_unpacked)
-
-    def build(self):
-        filename = systems[os_name]['libs'][self.lib_key]
-        if not os.path.isfile(filename):
-            self.uncompress()
-            depends = self.lib['compile']['depends']
-            [easy_install.main(['-U', dep]) for dep in depends]
-            title = '%s %s' % (os_name, p.architecture()[0])
-            bar = '-' * len(title)
-            print '+%s+\n|%s|\n+%s+' % (bar, title, bar)
-            import multiprocessing
-            # self.call('sudo rm %s' % filename)
-            path = '%s%s' % (tmp_path, self.lib['name'] % self.lib['version'])
-            config = self.lib['compile']['config']
-            ncores = multiprocessing.cpu_count()
-            self.call(('cd %s; %s ./configure %s; make -j %s; '
-                       ' sudo make install')
-                      % (path, config['pre'], config['post'], ncores))
-            update_shared_libs = systems[os_name]['update_shared_libs']
-            if update_shared_libs:
-                self.call(update_shared_libs)
-
-
-class build_wrapper(build):
-    def initialize_options(self):
-        # Deploy all the described libraries in the binaries dictionary.
-        libs = sorted(binaries.keys())
-        [Builder(lib).build() for lib in libs]
-        return build.initialize_options(self)
 
 
 setup(
@@ -201,7 +72,4 @@ setup(
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Scientific/Engineering :: Physics",
     ],
-    # cmdclass={
-    #    'build': build_wrapper,
-    #},
 )
