34877ace5ba3af7d5bd2d9f826772efab42501a5
eloy <eloy.colell@gmail.com>
2014-08-21 23:42:11 -0300

    Remove deprecated code from noaaclass/product/gvar_img.py.

--- noaaclass/product/gvar_img.py
+++ noaaclass/product/gvar_img.py
@@ -2,25 +2,6 @@ from noaaclass import core
 import re
 
 
-class Subscriber(object):
-    def row_to_dict(self, row):
-        elements = row.select('td')
-        _id = elements[3].a.text
-        enabled = (elements[1].renderContents().strip() == 'Yes')
-        result = (_id, {
-            'enabled': enabled,
-            'name': elements[2].a.text,
-            'edit': (self.item_url % (_id, 'Y' if enabled else 'N'))
-        })
-        return result
-
-    @property
-    def list(self):
-        rows = self.conn.get('subscriptions').select(
-            'table.class_table tr:nth-of-type(2)')
-        return dict([self.row_to_dict(r) for r in rows])
-
-
 class api(core.api):
     def register(self):
         self.name = 'GVAR_IMG'
