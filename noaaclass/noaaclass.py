bcacee0fc1e1358d2f1ea312d6ec41d0d63629c9
eloy <eloy.colell@gmail.com>
2014-08-23 10:30:36 -0300

    It incremented the requests timeout to 20 seconds.

--- noaaclass/noaaclass.py
+++ noaaclass/noaaclass.py
@@ -143,7 +143,7 @@ class Connection(object):
                                               headers=self.headers,
                                               cookies=self.cookies,
                                               allow_redirects=True,
-                                              timeout=2)
+                                              timeout=20)
         return self.last_response_soup
 
     def post(self, url, data, proto='http', form_name=None):
@@ -154,7 +154,7 @@ class Connection(object):
                                                cookies=self.cookies,
                                                data=form,
                                                allow_redirects=True,
-                                               timeout=2)
+                                               timeout=20)
         return self.last_response_soup
 
 
